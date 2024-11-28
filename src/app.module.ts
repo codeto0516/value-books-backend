54c4d81680f6cb5b119a5583aa3b0a632d927a7d
codeto0516 <codetaisei@gmail.com>
2024-11-28 20:09:04 +0900

    不要な記述を削除

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BookModule } from './modules/book/book.module';
-import { EmployeeModule } from './modules/employee/employee.module';
 
 @Module({
-  imports: [BookModule, EmployeeModule],
+  imports: [BookModule],
   controllers: [],
   providers: [],
 })
